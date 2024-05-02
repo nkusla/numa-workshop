103622961fe26d8e53f109b2902ec6b133b83570
nkusla <nikola.kuslakovic1616@gmail.com>
2024-05-02 19:18:33 +0200

    Added tasks for optimization with grad methods

--- notebooks/utils/test_functions.py
+++ notebooks/utils/test_functions.py
@@ -1,20 +1,20 @@
 import numpy as np
 
-def quadratic(x, a, b):
+def quadratic(x: np.ndarray, a: float, b: float):
 	return a*x[0]**2 + b*x[1]**2
 
-def quadratic_grad(x, a, b):
+def quadratic_grad(x: np.ndarray, a: float, b: float):
 	df_dx = 2*a*x[0]
 	df_dy = 2*b*x[1]
 	return np.array([df_dx, df_dy])
 
-def six_hump_camel_function(x):
+def six_hump_camel_function(x: np.ndarray)):
     term1 = (4 - 2.1 * x[0]**2 + (x[0]**4)/3) * x[0]**2
     term2 = x[0] * x[1]
     term3 = (-4 + 4 * x[1]**2) * x[1]**2
     return term1 + term2 + term3
 
-def six_hump_camel_gradient(x):
+def six_hump_camel_gradient(x: np.ndarray):
     df_dx = 2 * (x[0]**5 - 4.2 * x[0]**3 + 4 * x[0] + x[1])
     df_dy = 8 * (x[1]**3 - 2 * x[0]**2 * x[1])
     return np.array([df_dx, df_dy])
