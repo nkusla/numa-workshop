c45d0a1e24a560ac7ef6046a3fb699d8841dc05f
nkusla <nikola.kuslakovic1616@gmail.com>
2024-05-05 13:11:36 +0200

    Refactored some plot function

--- notebooks/utils/plot_utils.py
+++ notebooks/utils/plot_utils.py
@@ -1,20 +1,18 @@
 import numpy as np
 import matplotlib.pylab as plt
 from utils.numerical_algo import NumericalAlgo
-from typing import Tuple, Callable
+from typing import Tuple, Callable, List
 
 DIGITS_COUNT = 10
 LINSPACE_SAMPLE = 1000
 FIG_WIDTH = 15
 FIG_HEIGHT = 8
+CONTOUR_LEVELS = 20
 
 def plot_function2D(function: Callable, interval: Tuple[float, float]):
 	x = np.linspace(interval[0], interval[1], LINSPACE_SAMPLE)
 	y = function(x)
 	plt.plot(x, y, color="r")
-
-def plot_function3D(function: Callable, interval: Tuple[float, float]):
-	pass
 
 def set_plot_attributes(title: str, lim: Tuple[float, float]):
 	plt.figure(figsize=(15, 8))
@@ -28,7 +26,7 @@ def set_plot_attributes(title: str, lim: Tuple[float, float]):
 	plt.xlim(lim)
 	plt.ylim(lim)
 
-def visualize_bisection(algo: NumericalAlgo, lim: Tuple[float, float], animate=False):
+def visualize_bisection(algo: NumericalAlgo, lim: Tuple[float, float]):
 	solution = round(algo.result[-1], DIGITS_COUNT)
 	value = round(algo.function(solution), DIGITS_COUNT)
 	title = f"Bisection method\nIterations: {len(algo.result)}\nSolution: {solution}\nValue: {value}"
@@ -45,7 +43,7 @@ def visualize_bisection(algo: NumericalAlgo, lim: Tuple[float, float], animate=F
 
 	plt.show()
 
-def visualize_newton_rhapson(algo: NumericalAlgo, lim: Tuple[float, float], animate=False):
+def visualize_newton_rhapson(algo: NumericalAlgo, lim: Tuple[float, float]):
 	solution = round(algo.result[-1], DIGITS_COUNT)
 	value = round(algo.function(solution), DIGITS_COUNT)
 
@@ -63,20 +61,21 @@ def visualize_newton_rhapson(algo: NumericalAlgo, lim: Tuple[float, float], anim
 
 	plt.show()
 
-def visualize_secant(algo: NumericalAlgo, animate=False):
+def visualize_secant(algo: NumericalAlgo, lim: Tuple[float, float]):
 	pass
 
-def plot_run(algo: NumericalAlgo, x1_range: np.ndarray, x2_range: np.ndarray, title = "", levels=20, plot_3d=False):
+def plot_run(algo: NumericalAlgo, x_range: np.ndarray, plot_3d=False):
 	run = algo.result
 	function = algo.function
 	value = np.around(function(run[-1]), DIGITS_COUNT)
 	solution = np.around(run[-1], DIGITS_COUNT)
+	label = type(algo).__name__
 
-	title += f"\nIterations: {len(algo.result)}\n"
+	title = f"\nIterations: {len(algo.result)}\n"
 	title += f"Solution: {solution}\n"
 	title += f"Value: {value}"
 
-	X1, X2 = np.meshgrid(x1_range, x2_range)
+	X1, X2 = np.meshgrid(x_range, x_range)
 	Z = function([X1, X2])
 
 	x1 = np.array([x[0] for x in run])
@@ -90,16 +89,46 @@ def plot_run(algo: NumericalAlgo, x1_range: np.ndarray, x2_range: np.ndarray, ti
 		ax1 = fig.add_subplot(122, projection='3d')
 		ax1.scatter(x1, x2, z, c='black', marker='.', s=40)
 		ax1.plot_surface(X1, X2, Z, cmap='viridis', alpha=0.7)
+		ax1.legend([label], loc='upper right')
 		ax1.set_xlabel('x_1')
 		ax1.set_ylabel('x_2')
 		ax1.set_title('3D surface plot', fontsize=10)
 
 	ax2 = fig.add_subplot(121)
-	contour = ax2.contour(X1, X2, Z, levels=levels, alpha=1.0, cmap='viridis')
+	contour = ax2.contour(X1, X2, Z, levels=CONTOUR_LEVELS, alpha=1.0, cmap='viridis')
 	ax2.plot(x1, x2, color='black', marker='o', ls='--', linewidth=.5, ms=2)
+	ax2.legend([label], loc='upper right')
 	ax2.set_xlabel('x_1')
 	ax2.set_ylabel('x_2')
 	ax2.set_title('Contour plot', fontsize=10)
 
 	plt.tight_layout()
+	plt.show()
+
+def plot_multiple_runs(algos: List[NumericalAlgo], x_range: np.ndarray):
+	colors = ['k', 'b', 'g', 'r', 'c', 'm', 'y']
+
+	plt.figure(figsize=(FIG_HEIGHT, FIG_HEIGHT))
+
+	function = algos[0].function
+	X1, X2 = np.meshgrid(x_range, x_range)
+	Z = function([X1, X2])
+	plt.contour(X1, X2, Z, levels=CONTOUR_LEVELS, alpha=1.0, cmap='viridis')
+
+	plt.xlabel('x_1')
+	plt.ylabel('x_2')
+	plt.title('Contour plot', fontsize=10)
+
+	labels = [type(a).__name__ for a in algos]
+
+	for i, algo in enumerate(algos):
+		run = algo.result
+
+		x1 = np.array([x[0] for x in run])
+		x2 = np.array([x[1] for x in run])
+		z =  np.array([function(x) for x in run])
+
+		plt.plot(x1, x2, color=colors[i], marker='o', ls='--', linewidth=.5, ms=2, label=labels[i])
+
+	plt.legend(loc='upper right')
 	plt.show()
--- notebooks/utils/test_functions.py
+++ notebooks/utils/test_functions.py
@@ -8,7 +8,7 @@ def quadratic_grad(x: np.ndarray, a: float, b: float):
 	df_dy = 2*b*x[1]
 	return np.array([df_dx, df_dy])
 
-def six_hump_camel_function(x: np.ndarray)):
+def six_hump_camel_function(x: np.ndarray):
     term1 = (4 - 2.1 * x[0]**2 + (x[0]**4)/3) * x[0]**2
     term2 = x[0] * x[1]
     term3 = (-4 + 4 * x[1]**2) * x[1]**2
@@ -17,4 +17,12 @@ def six_hump_camel_function(x: np.ndarray)):
 def six_hump_camel_gradient(x: np.ndarray):
     df_dx = 2 * (x[0]**5 - 4.2 * x[0]**3 + 4 * x[0] + x[1])
     df_dy = 8 * (x[1]**3 - 2 * x[0]**2 * x[1])
+    return np.array([df_dx, df_dy])
+
+def plateau_function(x: np.ndarray):
+    return x[0]**2 + x[1]**2 - x[0]*x[1] + 5*x[1]**3 + 3*x[1]**4
+
+def plateau_function_grad(x: np.ndarray):
+    df_dx = 2*x[0] - x[1]
+    df_dy = 12*x[1]**3 + 15*x[1]**2 + 2*x[1] - x[0]
     return np.array([df_dx, df_dy])
